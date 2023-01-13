428dade5c37a23bdd7530e08e0dacc116c1db5ed
vipin <vipinkmundra@gmail.com>
2023-01-13 11:27:34 +0530

    solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      B b = new B();
+      String ans = b.meth();
+      System.out.println(ans);
+  }
+}
+
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
